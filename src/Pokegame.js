49f23620833f65fa42e64cf87dcbcd0c92aaa6dd
Appy Sharma <appy70890@gmail.com>
2020-02-08 17:46:52 +0100

    pokegame

---
+++ src/Pokegame.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Pokedex from "./Pokedex";
+
+class Pokegame extends Component {
+  render() {
+    return <Pokedex />;
+  }
+}
+
+export default Pokegame;
