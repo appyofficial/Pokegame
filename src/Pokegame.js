c70a68ae42f1672c89d3808c03c91087bb3126af
Appy Sharma <appy70890@gmail.com>
2020-02-08 17:56:56 +0100

    pokegame

--- src/Pokegame.js
+++ src/Pokegame.js
@@ -2,7 +2,26 @@ import React, { Component } from "react";
 import Pokedex from "./Pokedex";
 
 class Pokegame extends Component {
+  static defaultProps = {
+    pokemon: [
+      { id: 4, name: "Charmander", type: "fire", base_experience: 62 },
+      { id: 7, name: "Squirtle", type: "water", base_experience: 63 },
+      { id: 11, name: "Metapod", type: "bug", base_experience: 72 },
+      { id: 12, name: "Butterfree", type: "flying", base_experience: 178 },
+      { id: 25, name: "Pikachu", type: "electric", base_experience: 112 },
+      { id: 39, name: "Jigglypuff", type: "normal", base_experience: 95 },
+      { id: 94, name: "Gengar", type: "poison", base_experience: 225 },
+      { id: 133, name: "Eevee", type: "normal", base_experience: 65 }
+    ]
+  };
   render() {
+    let handOne = [];
+    let handTwo = [...this.props.pokemon];
+    while (handOne.length < handTwo.length) {
+      let randIdx = Math.random(Math.random() * handTwo.length);
+      let randPokemons = handTwo.splice(randIdx, 1)[0];
+      handOne.push(randPokemons);
+    }
     return <Pokedex />;
   }
 }
